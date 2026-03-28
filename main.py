93e10e68b8de27694205449233326946173a6d92
ahatuhov <ahatuhov@gmail.com>
2026-03-29 02:09:27 +0300

    simple app

--- main.py
+++ main.py
@@ -1,11 +1,69 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/", tags=["root"], summary="Главная страница")
+books = [
+    {
+        "id": 0,
+        "title": "My first book",
+        "author": "Ben",
+    },
+    {
+        "id": 1,
+        "title": "My second book",
+        "author": "Ann",
+    }
+]
+
+@app.get("/books", tags=["root"], summary="Получить все книги")
 def root():
-    return "Hello World"
+    return books
+
+
+@app.get("/books/{id}", tags=["root"], summary="Получить книгу по id")
+def get_book(id: int):
+
+    # return books[id]
+
+    for book in books:
+        if book["id"] == id:
+            return book
+    raise HTTPException(status_code=404, detail="Book not found")
+
+
+class NewBook(BaseModel):
+    # id: int
+    title: str
+    author: str
+
+
+@app.post("/books", tags=["root"], summary="Создать книгу")
+def create_book(book: NewBook):
+
+    # book = {
+    #     "title": "My new book",
+    #     "author": "John",
+    # }
+    # return book
+
+    books.append({
+        "id": len(books),
+        "title": book.title,
+        "author": book.author
+    })
+
+    for book in books:
+        if book["id"] == len(books)-1:
+            return book
+
+    raise HTTPException(status_code=422, detail="Book not found")
+
+
+    return book
+
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
